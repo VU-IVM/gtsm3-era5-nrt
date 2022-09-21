992ed6e12f291747e2682099e98664ac8735c609
Sanne Muis <muiss@int3.local.snellius.surf.nl>
2022-09-21 14:24:20 +0200

    updated scripts

---
+++ p1b_sbatch_download_tides.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -t 20:00:00
+#SBATCH -p thin
+#SBATCH -N 1
+
+# load modules
+module load 2021
+
+pdir="/gpfs/work1/0/einf3499/CDS_tides"
+# loop over months and years
+for yr in {2018..2025..1}; do
+  (
+    echo $tstart $pdir
+    conda run -n gtsm3-era5-nrt-slm python p1b_download_tides.py $yr $pdir
+  ) &
+  done
+done
+wait
--- p2_preprocess_ERA5.py
+++ p2_preprocess_ERA5.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
   
-def convert2FM(idir,tstart,tend):
+def convert2FM_yearly_runs(tstart,idir):
   """
   This function converts a ERA5 file from CDS to FM format
+  1. METEO-FORCING (yearly):
+	- Modify for correct longitude range [-180 to 180] and overlap
+	- Right var names and attributes
+	- Include spinup in yearly files (10 days)
+	- Set initial timesteps to zero to allow for SLR correction
   """
   # import modules
   import os
@@ -14,96 +19,83 @@ def convert2FM(idir,tstart,tend):
   # debug
   debug=False
   if debug==True:
-    tstart=dt.datetime.strptime('22/12/2021', '%d/%m/%Y')
-    tend=dt.datetime.strptime('01/01/2023', '%d/%m/%Y')
+    tstart=dt.datetime.strptime('01-01-2021', '%d-%m-%Y') 
     idir="/gpfs/work1/0/einf3499/meteo_ERA5"
+  # create variables 'dictionary' for filename pattern, variable name in ncin, variable name in ncout  
+  var_dict = {
+    "u10" : {
+      "standard_name" : "eastward_wind",
+      "long_name" : "10 metre U wind component",
+      "units" : "m s**-1",
+      "scale_factor" : float(0.01),
+      "offset" : float(0)},
+    "v10" : {
+      "standard_name" : "northward_wind",
+      "long_name" : "10 metre V wind component",
+      "units" : "m s**-1",
+      "scale_factor" : float(0.01),
+      "offset" : float(0)},
+    "msl" : {
+      "standard_name" : "air_pressure",
+      "long_name" : "Mean sea level pressure",
+      "units" : "Pa",
+      "scale_factor" : float(1),
+      "offset" : float(100000)}}              
+  # define ref/start/stop times 
+  spinup_period = [1,1,15,0] # imposed 1 day zero, 1 day transition, 15 days spinup
+  reftime=dt.datetime(1900,1,1) #imposed
+  date_start_zero = dt.datetime(tstart.year,1,1)-dt.timedelta(days=int(np.sum(spinup_period[0:3]))) #eg 15 dec
+  date_start_transition = date_start_zero+dt.timedelta(days=spinup_period[0]) #eg 16 dec
+  date_start_spinup = date_start_zero+dt.timedelta(days=spinup_period[0]+spinup_period[1]) #eg 17 dec
+  date_end = dt.datetime(tstart.year+1,1,1)
   # import dataset
-  no_files = len(glob.glob(os.path.join(idir,"ERA5_CDS_atm_%s*.nc" %(tstart.year))))
-  print('no. of files:', no_files)
   ds = xr.open_mfdataset(os.path.join(idir,"ERA5_CDS_atm_%s*.nc" %(tstart.year)))
-  spinup = dt.datetime.strptime('12-%s' %(tstart.year-1), "%m-%Y") 
-  ds_spinup = xr.open_mfdataset(os.path.join(idir,"ERA5_CDS_atm_%s*.nc" %(spinup.strftime('%Y-%m'))))
-  ds_merged = xr.combine_by_coords([ds, ds_spinup],compat='equals',combine_attrs='override').sel(time=slice(tstart,tend))
+  ds_spinup = xr.open_mfdataset(os.path.join(idir,"ERA5_CDS_atm_%s*.nc" %(date_start_zero.strftime('%Y-%m'))))
+  ds_merged = xr.combine_by_coords([ds, ds_spinup],compat='equals',combine_attrs='override').sel(time=slice(date_start_zero,date_end))
   # copy latitude and create new longitude
   lats = ds['latitude'][:]
   lons = ds['longitude'][:]
   part1 = (ds.longitude>178) #move 180:360 part to -180:0 so field now runs from longitute -180 to 180
   part2 = (ds.longitude<182) #take a bit of overlap to avoid interpolation issues at edge
   lons_new=np.hstack((lons[part1]-360,lons[part2]))
-  # times
-  reftime = dt.datetime(1900,1,1)
-  times = ds['time'][:]
-  ntimes = len(times)
+  # read times and create time
+  times = ds_merged['time'][:]
   dates = pd.to_datetime(pd.DataFrame({'year':times.dt.year, 'month':times.dt.month, 'day':times.dt.day, 'hour': times.dt.hour, 'minute':times.dt.minute, 'second':times.dt.second}))
-  rel_times = (dates-reftime) / np.timedelta64(1, 'h')
-  # copy to new dataset    
-  coords =  {'latitude': lats, 'longitude': lons_new, 'time': rel_times[0]}
-  # v10
-  datasets = []
-  for i in range(0,10):#len(rel_times)):
-    v10 = ds.v10.isel(time=i).values
-    var_new=np.hstack((v10[:,part1],v10[:,part2]))
-    da = xr.DataArray(var_new, coords=coords, dims=['latitude', 'longitude'])
-    da.name = 'v10'
-    dat = xr.concat([da], 'time')
-    datasets.append(dat)
-  ds_v10_merged = xr.concat(datasets, dim='time') 
-  print(ds_v10_merged)
-  # u10
-  datasets = []
-  for i in range(0,10):#len(rel_times)):
-    u10 = ds.u10.isel(time=i).values
-    var_new=np.hstack((u10[:,part1],u10[:,part2]))
-    da = xr.DataArray(var_new, coords=coords, dims=['latitude', 'longitude'])
-    da.name = 'u10'
-    dat = xr.concat([da], 'time')
-    datasets.append(dat)
-  ds_u10_merged = xr.concat(datasets, dim='time') 
-  # mslp
-  datasets = []
-  for i in range(0,10):#len(rel_times)):
-    msl = ds.msl.isel(time=i).values
-    var_new=np.hstack((msl[:,part1],msl[:,part2]))
-    da = xr.DataArray(var_new, coords=coords, dims=['latitude', 'longitude'])
-    da.name = 'msl'
-    dat = xr.concat([da], 'time')
-    datasets.append(dat)
-  ds_msl_merged = xr.concat(datasets, dim='time') 
-  # merge and set attributes
-  ds_all = xr.merge([ds_v10_merged,ds_u10_merged,ds_msl_merged]) 
-  # change attributes    
-  ds_all.u10.attrs['standard_name'] = 'eastward_wind'
-  ds_all.u10.attrs['long_name'] = '10 metre U wind component'
-  ds_all.u10.attrs['units'] = 'm s**-1'
-  ds_all.v10.attrs['standard_name'] = 'northward_wind'
-  ds_all.v10.attrs['long_name'] = '10 metre V wind component'
-  ds_all.v10.attrs['units'] = 'm s**-1'
-  ds_all.msl.attrs['standard_name'] = 'air_pressure'
-  ds_all.msl.attrs['long_name'] = 'Mean sea level pressure'
-  ds_all.msl.attrs['standard_name'] = 'Pa'
-  # user defined scaling factor and offset
-  scalef_msl=float(1)
-  scalef_u10=float(0.01)
-  scalef_v10=float(0.01) 
-  offset_msl=float(100000)
-  offset_u10=float(0)
-  offset_v10=float(0)
-  encoding = {}
-  encoding['msl'] = {'scale_factor': scalef_msl, 'add_offset': offset_msl}
-  encoding['u10'] = {'scale_factor': scalef_u10, 'add_offset': offset_u10}
-  encoding['v10'] = {'scale_factor': scalef_v10, 'add_offset': offset_v10}
-  ofile=os.path.join(idir.replace('meteo_ERA5','meteo_ERA5_fm'),"ERA5_CDS_atm_%s_%s.nc" %(dt.datetime.strftime(tstart, "%Y-%m-%d"),dt.datetime.strftime(tend, "%Y-%m-%d")))
-  print("Saving file as:",ofile)
-  ds_all.to_netcdf(ofile, encoding=encoding)
+  rel_times = (dates-reftime) / np.timedelta64(1, 'h') # relative time in hours since reftime
+  # copy v10, u10, msl to new dataset    
+  for varname in var_dict.keys():
+    datasets = []
+    for itime, time in enumerate(times):
+      var = ds_merged[varname].sel(time=time)
+      var_new = np.hstack((var[:,part1],var[:,part2]))
+      coords = {'latitude': lats, 'longitude': lons_new, 'time': time.values}
+      da = xr.DataArray(var_new, coords=coords, dims=['latitude', 'longitude'])
+      da.name = varname
+      dat = xr.concat([da],'time')
+      datasets.append(dat)
+    ds_var_merged = xr.concat(datasets, dim='time') 
+    ds_var_merged = ds_var_merged.where(ds_var_merged.time >= np.datetime64(date_start_spinup), 0) # values to zero for initalization SLR
+    bool = (ds_var_merged.time > np.datetime64(date_start_transition)) & (ds_var_merged.time < np.datetime64(date_start_spinup)) # select transition period
+    ds_var_merged = ds_var_merged.where(~bool, drop=True) # drop times for transition period
+    # set attributes + encoding
+    ds_var_merged.attrs['standard_name'] = var_dict[varname]['standard_name']
+    ds_var_merged.attrs['long_name'] = var_dict[varname]['long_name']
+    ds_var_merged.attrs['units'] = var_dict[varname]['units']
+    ds_var_merged['time'].attrs['units'] = "hours since %s" % (reftime)
+    comp = dict(scale_factor=var_dict[varname]['scale_factor'], add_offset=var_dict[varname]['offset'])
+    encoding = {varname: comp}
+    ofile=os.path.join(idir.replace('meteo_ERA5','meteo_ERA5_fm'),"ERA5_CDS_atm_%s_%s_%s.nc" %(varname, dt.datetime.strftime(tstart, "%Y-%m-%d"),dt.datetime.strftime(tend, "%Y-%m-%d")))
+    print("Saving file as:",ofile)
+    ds_var_merged.to_netcdf(ofile, encoding=encoding)
 
 if __name__ == "__main__":
     # read input arguments
     import os
-    import datetime
+    import datetime as dt
     if len(os.sys.argv)>0:
-      tstart=os.sys.argv[2]
-      tend=os.sys.argv[3]
-      outdir=os.sys.argv[1]        
+      tstart=dt.datetime.strptime(os.sys.argv[1], '%Y%m%d')
+      outdir=os.sys.argv[2]   
+      print(tstart)     
     else:
       raise RuntimeError('No arguments were provided\nFirst argument should indicate startdate as "%Y-%m-%d".\n Second argument for outdir. Script will download monthly files per day')
-    convert2FM(tstart,tend,outdir)
+    convert2FM_yearly_runs(tstart,outdir)
