7fdb9909741a2f72612c567ca117739fb9a8fa13
veenstrajelmer <veenstrajelmer@gmail.com>
2023-06-08 14:05:21 +0200

    removed extension scripts, merged with non-extension

--- p1a_sbatch_download_era5.sh
+++ p1a_sbatch_download_era5.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -t 1:00:00
+#SBATCH -t 20:00:00
 #SBATCH -p thin
 #SBATCH -N 1
 
@@ -9,7 +9,7 @@ module load 2021
 pdir="/gpfs/work1/0/einf3499/meteo_ERA5_extended"
 # loop over months and years
 for i in {1979..1979..1}; do
-  for j in {1..1..1}; do
+  for j in {1..12..1}; do
   (
     tstart=$(date -d ""$(printf -- '%04d' "$i" )"-"$(printf -- '%02d' "$j" )"-01" +'%d-%m-%Y')
     echo $tstart $pdir
