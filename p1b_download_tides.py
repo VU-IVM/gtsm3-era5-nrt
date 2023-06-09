9247f7cbb15a77e242b1107b5a22ff81a7bfecf9
veenstrajelmer <veenstrajelmer@gmail.com>
2023-06-09 13:27:43 +0200

    simplified download_tides

--- p1b_download_tides.py
+++ p1b_download_tides.py
@@ -1,37 +1,37 @@
 #!/usr/bin/env python
 
 import os
-import datetime as dt
 import cdsapi
 import zipfile
 
 
 def download_tides(yr, mnth, outdir):
-    # Get current date, for month and year information
-    tdate = dt.datetime(yr,mnth,1).date()
+    date_str = f'{yr}-{mnth:02d}'
+    os.makedirs(outdir,exist_ok=True)
     # Monthly download
     print ("######### GTSM-tides from CDS  #########")
-    print ('get data from ', tdate)
-    print ("################################")
-    output_path = os.path.join(outdir, f'future_tide_{tdate.year}_{tdate.month:02}_v1.nc')
-    if os.path.isfile(output_path)==False: 
-        filename = f'download_{tdate}.zip'
-        output_path = os.path.join(outdir, filename)
+    print (f'getting data for {date_str}')
+    experiment = 'historical' #historical/future
+    path_ncfile = os.path.join(outdir, f'{experiment}_tide_{yr}_{mnth:02}_v1.nc')
+    if os.path.isfile(path_ncfile):
+        print('already downloaded')
+    else:
+        path_zipfile = os.path.join(outdir, f'download_{date_str}.zip')
         c = cdsapi.Client()
         c.retrieve(
             'sis-water-level-change-timeseries-cmip6',
             {
                 'format': 'zip',
                 'variable': 'tidal_elevation',
-                'experiment': 'historical',
-                'year': tdate.year,
-                'month':["{:02}".format(tdate.month)],
+                'experiment': experiment,
+                'year': yr,
+                'month':[f'{mnth:02}'],
                 'temporal_aggregation': '10_min',
-            }, output_path)
-        # unzip
-        with zipfile.ZipFile(output_path,"r") as zip_ref:
+            }, path_zipfile)
+        # unzip and remove zipfile
+        with zipfile.ZipFile(path_zipfile,"r") as zip_ref:
             zip_ref.extractall(outdir)
-        os.remove(output_path)
+        os.remove(path_zipfile)
     
     
 if __name__ == "__main__":
@@ -41,5 +41,8 @@ if __name__ == "__main__":
         mnth = os.sys.argv[2]
         outdir = os.sys.argv[3]
     else:
-        raise RuntimeError('No arguments were provided\nFirst argument should indicate year. Second argument should indicate month. Third argument for outdir. Script will download monthly files per day')
+        yr = 1960
+        mnth = 1
+        outdir = './tides_CDS'
+        # raise RuntimeError('No arguments were provided\nFirst argument should indicate year. Second argument should indicate month. Third argument for outdir. Script will download monthly files per day')
     download_tides(yr,mnth,outdir)
