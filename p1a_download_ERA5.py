aa9cb1655879f2ca0a1cfac8d5c28fd270e8267f
veenstrajelmer <veenstrajelmer@gmail.com>
2023-06-09 12:31:03 +0200

    improved prints and cleanups

--- p1a_download_ERA5.py
+++ p1a_download_ERA5.py
@@ -13,12 +13,10 @@ def download_era5(tstart,outdir):
     dayarray = ["{:02}".format(iday) for iday in range(1,lastday+1)]     
     # daily download
     print ("######### ERA-5 from CDS  #########")
-    print ('get data from ', tstart,'(1 Day)')
-    print ("################################")
     os.makedirs(outdir,exist_ok=True)
     # I/O - download the data
     for day in dayarray:
-        print(day)
+        print (f'getting data for {tstart.year}-{tstart.month:02d}-{day}')
         tday = dt.datetime(day=int(day),month=tstart.month,year=tstart.year).date()
         targetfile = os.path.join(outdir,"ERA5_CDS_atm_%s.nc" %(tday))
         if os.path.exists(targetfile):
@@ -31,7 +29,7 @@ def download_era5(tstart,outdir):
                 'format':'netcdf',
                 'variable':['10m_u_component_of_wind','10m_v_component_of_wind','mean_sea_level_pressure'],
                 'year':tstart.year,
-                'month':["{:02}".format(tstart.month)],
+                'month':[f"{tstart.month:02}"],
                 'day':day,
                 'time':['00:00','01:00','02:00','03:00','04:00','05:00',
                         '06:00','07:00','08:00','09:00','10:00','11:00',
