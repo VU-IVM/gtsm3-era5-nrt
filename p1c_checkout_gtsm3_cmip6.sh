9511e5311a6bc1a4ff4915fd4b2f93c6538c9606
Natalia Aleksandrova <naleksandro@tcn426.local.snellius.surf.nl>
2023-07-21 15:53:17 +0200

    fixed directory referencing

--- p1c_checkout_gtsm3_cmip6.sh
+++ p1c_checkout_gtsm3_cmip6.sh
@@ -8,13 +8,12 @@ module load 2021
 #get dir_modelfiles as variable
 dir_modelfiles=$(python path_dict.py modelfiles)
 
-mkdir "$dir_modelfiles"
-#cd dir_modelfiles
+mkdir $dir_modelfiles
 
 #checkout gtsm3_cmip6 repos folder to dir_modelfiles
-svn checkout https://repos.deltares.nl/repos/global_tide_surge_model/trunk/gtsm3_cmip6 "$dir_modelfiles"
+svn checkout https://repos.deltares.nl/repos/global_tide_surge_model/trunk/gtsm3_cmip6 $dir_modelfiles
 
 #remove non-template ext/mdu/sh files to avoid issues/confusion
-rm "$dir_modelfiles"/*.ext
-rm "$dir_modelfiles"/*.mdu
-rm "$dir_modelfiles"/*.sh
+rm $dir_modelfiles/*.ext
+rm $dir_modelfiles/*.mdu
+rm $dir_modelfiles/*.sh
