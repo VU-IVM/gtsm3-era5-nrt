2a5b7f643a0192865125104dff24fabdcf78824e
Sanne Muis <sanne.muis@deltares.nl>
2023-08-25 14:57:15 +0200

    add script to run notebook on snellius

---
+++ analysis/run_jupyter.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -t 4:00:00
+#SBATCH -N 1
+
+# Make sure the jupyter command is available, either by loading the appropriate modules, sourcing your own virtual environment, etc.
+module load 2021
+
+source /home/muiss/miniconda3/etc/profile.d/conda.sh
+conda activate /home/muiss/miniconda3/envs/era5-e
+ 
+# Choose random port and print instructions to connect
+PORT=`shuf -i 5000-5999 -n 1`
+LOGIN_HOST=${SLURM_SUBMIT_HOST}-pub.snellius.surf.nl
+BATCH_HOST=$(hostname)
+ 
+echo "To connect to the notebook type the following command from your local terminal:"
+echo "ssh -J ${USER}@${LOGIN_HOST} ${USER}@${BATCH_HOST} -L ${PORT}:localhost:${PORT}"
+echo
+echo "After connection is established in your local browser go to the address:"
+echo "http://localhost:${PORT}"
+
+#jupyter notebook --no-browser --port $PORT
+jupyter lab --no-browser --port ${PORT}
+#jupyter lab --no-browser --port ${port} 
