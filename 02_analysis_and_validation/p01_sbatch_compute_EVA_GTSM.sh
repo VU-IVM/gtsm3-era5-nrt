9b763825e3b4a7131b033b60ccb3e4bff329c350
Natalia Aleksandrova <naleksandro@int5.local.snellius.surf.nl>
2024-08-15 11:53:41 +0200

    including 2023 in the analysis period

--- 02_analysis_and_validation/p01_sbatch_compute_EVA_GTSM.sh
+++ 02_analysis_and_validation/p01_sbatch_compute_EVA_GTSM.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -t 20:00:00
 #SBATCH -N 1
-#SBATCH -n 128
-#SBATCH -p rome
+#SBATCH -n 192
+#SBATCH -p genoa
 #SBATCH --job-name=eva_gtsm
 
 # load modules
@@ -12,11 +12,11 @@ module load 2021
 step=100
 # on large node, 128 cores, it is possible to run 90 batches of 100 stations without running out of memory. When using a different machine, the limits will be different.
 # submit analysis of extremes in batches per 100 stations
-for st_start in {0..8900..100}; do
+for st_start in {0..2400..100}; do
 (
   st_end=$(( $st_start + $step - 1 ))
   # inputs to the function are start year, end year, station number range and which timeseries to use (1-hour or 10-min, where recommended default is 1-hour due to large memory needed)
-  conda run -n gtsm3-era5-nrt-slm python p02_computing_return_periods_GTSM_batch_pyextremes.py 1950 2022 $st_start $st_end "1hr"
+  conda run -n gtsm3-era5-nrt-slm python p01_computing_return_periods_GTSM_batch_pyextremes.py 1950 2023 $st_start $st_end "1hr"
 ) &
 done
 wait
