8352bbca90a483a05f435dfdd439059f31bc2b01
Natalia Aleksandrova <naleksandro@int5.local.snellius.surf.nl>
2024-09-03 17:25:57 +0200

    updated partition name on Snellius

--- 01_model_simulations/p4_sbatch_postprocess.sh
+++ 01_model_simulations/p4_sbatch_postprocess.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH -t 20:00:00
 #SBATCH -N 1
-#SBATCH -n 16
-#SBATCH -p fat
+#SBATCH -n 128
+#SBATCH -p rome
 #SBATCH --job-name=p4_sbatch_postprocess
 
 # load modules
 module purge
 module load 2021
 
-# settings
+# settings - Note: for 2 years of data you need 80 cores on rome partition
 scenario=era5
 # yearly runs for 2019, 2020 and 2021
 for yr in {2023..2023..1}; do
